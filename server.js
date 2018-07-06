65f2321b6ddeaa97d445033a242a5e960306ebff
Kellie Dambroso <kelliedambroso@Kellies-MacBook-Pro.local>
2018-07-06 10:28:39 -0500

    updating package.json and updating code for heroku

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('this works');
+const express= require('express');
+const app = express();
+const PORT= process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+  res.send("this works");
+})
+
+
+app.listen(PORT, ()=>{
+
+  console.log('this works');
+})
